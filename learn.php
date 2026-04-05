529d5beff9f1c042a0550dac4cfde5687ccc117f
Marvin <marvinbernard2018@gmail.com>
2026-04-05 08:42:53 +0300

    Added html tag and proper use of $ when creatnig an object

--- learn.php
+++ learn.php
@@ -1,3 +1,4 @@
+<html>
 <?php
 // 1.Variables (Data Storage)
 
@@ -8,21 +9,24 @@ $isStudent = true; //boolean
 
 // 2.Classes (Blueprints)
 
-class Student{
+class Students{
     public $name; //property
     public $age; //property
 
 
     public function register (){ // method
-        echo "Student register";
+        echo "Student register<br>";
     }
 }
 
 // Create an object and call the method
-$student1 = new Student(); // create object
+$student1 = new Students(); // create object
 $student1->name = "Marvin";
 $student1->age = 22;
 $student1->register(); // Output: Student register
+
+?>
+<?php
 
 // 3.Functions and methods
 
@@ -35,7 +39,9 @@ class Calculator {
         return $a + $b;
     }
 }
+?>
 
+<?php
 
 // 4.Encapsulation 
 
@@ -43,7 +49,7 @@ class BankAccount {
     private $balance = 0;
 
     public function deposit($amount){
-        if($amount > 0);{
+        if($amount > 0){
         $this->balance += $amount;
         }
     }
@@ -53,3 +59,38 @@ class BankAccount {
 }
 
 ?>
+
+<?php
+// 5.Inheritance 
+
+/*Inheritance allows one class to reuse and extend another class’s 
+properties and methods.
+- use{extends} keyword in php
+
+- Child class inherits everything from parent, but 
+can add or override */
+class Person {
+    public $name;
+
+    public function greet(){
+        echo "Hello, my name is $this->name<br>";
+    }
+}
+
+class Student extends Person {
+    public $studentId;
+
+    public function showId(){
+        echo "My student ID is $this->studentId<br>";
+
+    }
+}
+
+$Person = new Student();
+$Person->name = "Marvin";
+$Person->studentId = "A-001";
+$Person->greet();
+$Person->showId();
+
+?>
+</html>
