92989e8264293ebd35e86134ec144fc80321ae8e
Marvin <marvinbernard2018@gmail.com>
2026-04-05 07:04:19 +0300

    Learn to use encapsulation

--- learn.php
+++ learn.php
@@ -1,5 +1,5 @@
 <?php
-// Variables (Data Storage)
+// 1.Variables (Data Storage)
 
 $name = "Marvin"; //string
 $age = 22; //integer
@@ -8,7 +8,7 @@ $isStudent = true; //boolean
 ?>
 
 <?php
-// Classes (Blueprints)
+// 2.Classes (Blueprints)
 
 class Student{
     public $name; //property
@@ -25,5 +25,38 @@ $student1 = new Student(); // create object
 $student1->name = "Marvin";
 $student1->age = 22;
 $student1->register(); // Output: Student register
+
+?>
+
+<?php
+// 3.Functions and methods
+
+// function($name){
+//     return "Hello, $name";
+// }
+
+class Calculator {
+    public function add($a, $b){
+        return $a + $b;
+    }
+}
+
+?>
+
+<?php
+// 4.Encapsulation 
+
+class BankAccount {
+    private $balance = 0;
+
+    public function deposit($amount){
+        if($amount > 0);{
+        $this->balance += $amount;
+        }
+    }
+    public function getBalance(){
+        return $this->balance;
+    }
+}
 
 ?>
